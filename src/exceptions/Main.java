9b5ce1cc2efbbbc42e814dfa079fe12a1b260214
Milica Simović <85285657+MiliSii@users.noreply.github.com>
2022-07-12 16:31:27 +0200

    test

--- src/exceptions/Main.java
+++ src/exceptions/Main.java
@@ -4,6 +4,15 @@ import java.nio.channels.ScatteringByteChannel;
 import java.util.Scanner;
 
 public class Main {
+    static void checkAge(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        }
+        else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+
     public static void main(String[] args) {
         //TRY-CATCH
 
