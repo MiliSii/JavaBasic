cbcf06989b4d1a7645482c4e7a7a14b1ac7ade03
Milica Simović <85285657+MiliSii@users.noreply.github.com>
2022-07-13 11:50:14 +0200

    test

--- src/exceptions/Main.java
+++ src/exceptions/Main.java
@@ -5,8 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.channels.ScatteringByteChannel;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 import static java.lang.Class.forName;
 import static java.lang.reflect.Array.newInstance;
@@ -15,6 +14,110 @@ public class Main {
 
 
 
+
+
+    public static void m(){
+        int number=0;
+        try {
+            Scanner s=new Scanner(System.in);
+            System.out.println("input a number: ");
+            number=s.nextInt();
+            System.out.println(number);
+
+            // metoda koja vrsi citanje unosa i/o
+           // unos();
+        } catch (Exception e1){
+            // obraditi izuzetak
+            System.out.println("Input is not a number");
+        } catch (Throwable e){
+            System.out.println(e.getMessage());
+
+        }finally {
+            System.out.println("entered number is "+number);
+        }
+
+    }
+
+    // funkcija koja proverava unos i baca izuzetak -
+
+
+
+    public static void t(){
+        try {
+            int array[] = new int[10];
+            array[10] = 30 / 0;
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static  void ListOfNumbers(){
+        try {
+            Integer[] n ={1,2};
+            System.out.println(n[8]);
+        }catch (Exception e){
+            System.out.println("an exception happend!");
+        }
+
+    }
+
+
+
+
+
+    //CHECKED EXCEPTIONS
+    //Java verifies checked exceptions at compile-time.
+    //a method must handle in its body or throw to the caller method so the caller method can handle it.
+    //Checked exceptions are checked by the Java compiler so they are called compile time exceptions.
+
+    public static void exampleCh() throws ClassNotFoundException {
+        forName("exceptions-mili");
+    }
+
+    public static void readFile(String fileName){
+        try {
+            FileReader reader=new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+            ////Alternate logic
+            //		e.printStackTrace();
+        }
+
+    }
+
+
+
+    public static void l(){
+        try
+        {
+            FileReader file = new FileReader("src/exceptions/test.txt");
+
+            file.read();
+
+        }
+        catch (FileNotFoundException e)
+        {
+            throw new RuntimeException("Incorrect path to the file");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    //UNCHECKED EXCEPTIONS
+
+
+    public static void multiple(){
+        try {
+            String s = null;
+            System.out.println(s.length());
+        } catch (Exception e) {
+            throw new NullPointerException(e.getMessage());
+        }
+
+    }
     public static void InputNumber(){
         int number=0;
         int number1=0;
@@ -39,85 +142,6 @@ public class Main {
         }
 
     }
-
-    public static void m(){
-        int number=0;
-        try {
-            Scanner s=new Scanner(System.in);
-            System.out.println("input a number: ");
-            number=s.nextInt();
-            System.out.println(number);
-        } catch (Exception e1){
-            System.out.println("input must be number");
-            Scanner s=new Scanner(System.in);
-            System.out.println("input a number: ");
-            number=s.nextInt();
-            System.out.println(number);
-        } catch (Throwable e){
-            System.out.println(e.getMessage());
-
-        }finally {
-            System.out.println("entered number is "+number);
-        }
-
-    }
-
-
-    public static void t(){
-        try {
-            int array[] = new int[10];
-            array[10] = 30 / 0;
-        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public static  void ListOfNumbers(){
-        try {
-            Integer[] n ={1,2};
-            System.out.println(n[8]);
-        }catch (Exception e){
-            System.out.println("an exception happend!");
-        }
-
-    }
-
-    public static void multiple(){
-        try {
-            String s = null;
-            System.out.println(s.length());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-
-
-    //CHECKED EXCEPTIONS
-    //Java verifies checked exceptions at compile-time.
-    //a method must handle in its body or throw to the caller method so the caller method can handle it.
-    //Checked exceptions are checked by the Java compiler so they are called compile time exceptions.
-
-    public static void exampleCh() throws ClassNotFoundException {
-        forName("exceptions-mili");
-    }
-
-    public static void readFile(String fileName){
-        try {
-            FileReader reader=new FileReader(fileName);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-
-            ////Alternate logic
-            //		e.printStackTrace();
-        }
-
-    }
-    public static void x(){
-
-    }
-
     static void checkAge(int age) {
         if (age < 18) {
             throw new ArithmeticException("Access denied - You must be at least 18 years old.");
@@ -128,29 +152,35 @@ public class Main {
 
     }
 
-   /* public static void Object newInstance(){
+    static void j(){
+        try {
+            int[] myArray = {1,2,3,4,5};
+            System.out.println("Elements in the array are: ");
+            System.out.println(Arrays.toString(myArray));
+            Scanner s = new Scanner(System.in);
 
-    }*/
+            System.out.println("Enter the index of the required element: ");
+            int element = s.nextInt();
 
-    //UNCHECKED EXCEPTIONS
-public static void l(){
-    try
-    {
-        //FileReader file = new FileReader("test.txt");
-
-        //file = null;
-
-       // file.read();
-        Scanner input = new Scanner(new File("word.txt"));
-        String in = "";
-        in = input.nextLine();
+            System.out.println("Element in the given index is :: "+myArray[element]);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException(e.getMessage());
+        }
     }
-    catch (IOException e)
-    {
-        //Alternate logic
-        e.printStackTrace();
+
+    public static void texampleIllegalStateException(){
+        try {
+            ArrayList<String> list = new ArrayList<String>();
+            list.add("a");
+            list.add("b");
+            list.add("c");
+
+            Iterator<String> it = list.iterator();
+            it.remove();
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e.getMessage());
+        }
     }
-}
 
 
     public static void main(String[] args) {
@@ -159,85 +189,12 @@ public static void l(){
         //InputNumber();
         //readFile("New Microsoft Word Document.txt");
         //multiple();
-         // m();
+        // m();
         //x();
-        l();
+        //l();
+        //j();
+        //texampleIllegalStateException();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      /*  try {
-            int[] i={8,8,8};
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-        }*/
-
-
-         /*  int[] myNumbers = {1, 2, 3};
-        System.out.println(myNumbers[10]); // error!
-
-
-        try {
-            int[] myNumbers1 = {1, 2, 3};
-            System.out.println(myNumbers1[10]);
-        } catch (Exception e) {
-            System.out.println("Something went wrong.");
-        }
-
-
-       try {
-            int[] myNumbers3 = {1, 2, 3};
-            System.out.println(myNumbers3[10]);
-        } catch (Exception e) {
-            System.out.println("Something went wrong.");
-        } finally {
-            System.out.println("The 'try catch' is finished.");
-        }
-
-
-          checkAge(15); // Set age to 15 (which is below 18...)
-          checkAge(20);//working
-*/
-//program to print the exception information using printStackTrace() method
-      /*  int a=5;
-        int b=0;
-        try{
-            System.out.println(a/b);
-        }
-        catch(ArithmeticException e){
-            e.printStackTrace();
-        }
-
-//program to print the exception information using toString() method
-        try{
-            System.out.println(a/b);
-        }
-        catch(ArithmeticException e){
-            System.out.println(e.toString());
-        }
-
-        //program to print the exception information using getMessage() method
-        try{
-            System.out.println(a/b);
-        }
-        catch(ArithmeticException e){
-            System.out.println(e.getMessage());
-        }*/
 
     }
 
