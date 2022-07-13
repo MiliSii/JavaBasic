8f8cac8cafa7eacd74631a0b398e0391f6cdd262
Milica Simović <85285657+MiliSii@users.noreply.github.com>
2022-07-13 08:50:55 +0200

    test

--- src/exceptions/Main.java
+++ src/exceptions/Main.java
@@ -1,21 +1,40 @@
 package exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.nio.channels.ScatteringByteChannel;
 import java.util.Scanner;
 
+import static java.lang.Class.forName;
+import static java.lang.reflect.Array.newInstance;
+
 public class Main {
-    static void checkAge(int age) {
-        if (age < 18) {
-            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+    /* public static void email(){
+             Scanner s=new Scanner(System.in);
+             System.out.println("input a email: ");
+             String email=s.next();
+             System.out.println(email);
+     }
+
+ */
+    public static void InputNumber(){
+        int number=0;
+        try {
+            Scanner s=new Scanner(System.in);
+            System.out.println("input a number: ");
+            number=s.nextInt();
+            System.out.println(number);
+
+        }catch (Exception e1){
+
+            System.out.println("input must be number");
+        }     finally {
+            System.out.println("entered number is "+number);
         }
-        else {
-            System.out.println("Access granted - You are old enough!");
-        }
+
     }
 
-    public static void main(String[] args) {
-        //TRY-CATCH
-
+    public static  void ListOfPeople(){
         try {
             String[] people ={"milica","ana"};
             System.out.println(people[8]);
@@ -23,25 +42,78 @@ public class Main {
             System.out.println("an exception happend!");
         }
 
+    }
 
+
+
+    //CHECKED EXCEPTIONS
+    //Java verifies checked exceptions at compile-time.
+    //a method must handle in its body or throw to the caller method so the caller method can handle it.
+    //Checked exceptions are checked by the Java compiler so they are called compile time exceptions.
+
+    public static void exampleCh() throws ClassNotFoundException {
+        forName("exceptions-mili");
+    }
+
+    public static void readFile(String fileName){
         try {
-            Scanner s=new Scanner(System.in);
-            System.out.println("input number: ");
-            int number=s.nextInt();
-            System.out.println(number);
-
-        }catch (Exception e1){
-
-            System.out.println("input must be number");
+            FileReader reader=new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
 
+    }
 
-        try {
+    static void checkAge(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        }
+        else {
+            System.out.println("Access granted - You are old enough!");
+        }
+
+    }
+
+   /* public static void Object newInstance(){
+
+    }*/
+
+
+
+
+    public static void main(String[] args) {
+
+
+        InputNumber();
+
+//readFile("New Microsoft Word Document.txt");
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //TRY-CATCH
+
+
+
+
+
+
+
+      /*  try {
             int[] i={8,8,8};
         } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
-        }
+        }*/
 
 
          /*  int[] myNumbers = {1, 2, 3};
@@ -70,7 +142,7 @@ public class Main {
           checkAge(20);//working
 */
 //program to print the exception information using printStackTrace() method
-        int a=5;
+      /*  int a=5;
         int b=0;
         try{
             System.out.println(a/b);
@@ -93,7 +165,7 @@ public class Main {
         }
         catch(ArithmeticException e){
             System.out.println(e.getMessage());
-        }
+        }*/
 
     }
 
